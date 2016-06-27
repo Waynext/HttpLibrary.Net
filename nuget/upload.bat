af6a386363636a88c89cad00a7430ac388bf56dc
Wayne Gu <wgu@nero.com>
2016-06-27 16:33:15 +0800

    Set version 0.0.2 and update nuget target frameworks

--- nuget/upload.bat
+++ nuget/upload.bat
@@ -1,2 +1,2 @@
-..\.nuget\nuget push output\HttpLibrary.Net.Core-PCL.0.0.1.nupkg
-..\.nuget\nuget push output\HttpLibrary.Net-PCL.0.0.1.nupkg
+..\.nuget\nuget push output\HttpLibrary.Net.Core-PCL.0.0.2.nupkg
+..\.nuget\nuget push output\HttpLibrary.Net-PCL.0.0.2.nupkg
