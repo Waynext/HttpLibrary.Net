2b381a1c3e8b3dd41f6aabcd8ff87bd594eba4cc
Wayne Gu <wgu@nero.com>
2016-06-29 19:03:38 +0800

    Set version 0.0.5.0

--- nuget/upload.bat
+++ nuget/upload.bat
@@ -1,2 +1,2 @@
-..\.nuget\nuget push output\HttpLibrary.Net.Core-PCL.0.0.4.nupkg
-..\.nuget\nuget push output\HttpLibrary.Net-PCL.0.0.4.nupkg
+..\.nuget\nuget push output\HttpLibrary.Net.Core-PCL.0.0.5.nupkg
+..\.nuget\nuget push output\HttpLibrary.Net-PCL.0.0.5.nupkg
