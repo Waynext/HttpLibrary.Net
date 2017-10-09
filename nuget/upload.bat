6960babf158e764acd041d1df271d442360f1fd6
gu wayne <Wayne@cn-c-w-652.nero-cn.nero-de.internal>
2017-10-09 16:40:41 +0800

    set version 1.0.0

--- nuget/upload.bat
+++ nuget/upload.bat
@@ -1,2 +1,2 @@
-..\.nuget\nuget push output\HttpLibrary.Net.Core-PCL.0.0.7.nupkg
-..\.nuget\nuget push output\HttpLibrary.Net-PCL.0.0.7.nupkg
+..\.nuget\nuget push output\HttpLibrary.Net.Core-PCL.0.1.0.nupkg
+..\.nuget\nuget push output\HttpLibrary.Net-PCL.0.1.0.nupkg
